37da36f559d1356287308d2e8b6a49fc5794d10c
James <locklear.james.jl@gmail.com>
2024-08-06 04:50:09 -0400

    got getHumanChoice function working properly

--- js/script.js
+++ js/script.js
@@ -3,4 +3,7 @@ function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * 3) + 1;
   return choices[randomIndex];
 }
-console.log(getComputerChoice());
+function getHumanChoice() {
+  return prompt("rock paper or scissors?");
+}
+console.log(getHumanChoice());
